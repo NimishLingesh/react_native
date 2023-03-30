f0f9daae60716ea995e1044fdcafe0b9820307c3
Spartan <nimish.lingesh@gmail.com>
2023-03-30 00:10:46 -0700

    1. ScrollView   2. FlatList

--- my-app-2/components/FlexLayout.js
+++ my-app-2/components/FlexLayout.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-import {Button, FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, FlatList, StyleSheet, Text, TextInput, View, ScrollView} from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const Separator = () => <View style={styles.separator} />;
 
 const FlexLayout = props => {
     const [enteredText, setEnteredText] = useState('');
+    // this is just for the FlatList which uses the object of each of the list items
+    const [textForFlatList, setTextForFlatList] = useState('')
     const [textList, setTextList] = useState([]);
 
     function textInputHandler(textValue) {
@@ -11,9 +15,19 @@ const FlexLayout = props => {
     }
 
     function addTextHandler() {
-        console.log(textList);
+        // this is how you concatenate lists
         setTextList((newText) => [...newText, enteredText]);
+
+        // key here is automatically determined by the FlatList
+        setTextForFlatList((newText) => [
+          ...newText, 
+          // {text: enteredText, key: Math.random().toString()},
+          // Instead of key, if 'id' is being used then keyExtractor parameter in the FlatList would be utilized
+          {text: enteredText, id: Math.random().toString()},
+        ]);
+
         setEnteredText('');
+        console.log(textList);
         // console.log(enteredText)
     }
 
@@ -28,10 +42,38 @@ const FlexLayout = props => {
             <Button title='Add' style={{flex: 1}} onPress={addTextHandler}></Button>
         </View>
 
+        {/* wrapping the text within View is not essential. But to make it adjust to differnt Platform like ios we do that here  */}
         <View>
+        
+        <ScrollView style={styles.listingContainer} nestedScrollEnabled={true}> 
             {/* need to define key here to uniquely identify the list  */}
-            {textList.map((txt) => <Text key={txt}>{txt}</Text>)}
+            {textList.map((txt) => (
+            <View> 
+              <Text style={styles.txtLst}key={txt}>{txt}</Text>
+            </View>
+            ))}
+        </ScrollView>
+
+        {/* </ Separator> */}
+
+        <Text>FLATLIST</Text>
+
+        <FlatList data={textForFlatList} renderItem={(itemData) => {
+          return (
+            <View> 
+            <Text style={styles.txtLst}>{itemData.index+1}. {itemData.item.text}</Text>
+            </View>
+          );
+        }}
+        keyExtractor={(item, index) => {
+          return item.id;
+        }} 
+        alwaysBounceVertical={false}> 
+          
+        </FlatList> 
         </View>
+        
+
     </View>
   );
 };
@@ -49,7 +91,11 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         // alignItems: 'stretch'
     },
+    listingContainer: {
+        flex: 5
+    },
     textInput: {
+        // see the ration of the flex here and the button
         flex: 3,
         margin: 1,
         borderWidth: 2,
@@ -62,6 +108,13 @@ const styles = StyleSheet.create({
       fontSize: 18,
       height: 44,
     },
+    txtLst: {
+      margin: 8,
+      padding: 8,
+      borderRadius: 6,
+      backgroundColor: '#5e0acc',
+      color: 'white',
+    }
   });
 
 export default FlexLayout;
